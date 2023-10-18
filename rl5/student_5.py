f44ed26467e62d219c8a347633450dab28b89184
Vicky Liu <u3532975@connect.hku.hk>
2023-10-18 23:58:03 +0200

    Finish RL Assignment

---
+++ rl5/student_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Q learning learning rate
+alpha = 0.4
+
+# Q learning discount rate
+gamma = 0.2
+
+# Epsilon initial
+epsilon_initial = 1
+
+# Epsilon final
+epsilon_final = 0.2
+
+# Annealing timesteps
+annealing_timesteps = 1
+
+# threshold
+threshold = 1e-6
